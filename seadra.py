49db01039526906763240be00cbbe2b756b3df81
actionless <actionless.loveless@gmail.com>
2014-11-25 21:47:04 +0100

    style: cleaned-up a bit; renamed some methods and variables for more readability

--- seadra.py
+++ seadra.py
@@ -2,21 +2,20 @@
 import sys
 import os
 import urllib.parse
-from PyQt5.QtWebKitWidgets import QWebView, QWebPage
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import QtCore
-Qt = QtCore.Qt
-from PyQt5.QtGui import QPalette
-import PyQt5.QtDBus as QtDBus
 from subprocess import Popen, PIPE
 
+from PyQt5.QtCore import Qt, QUrl, QObject, pyqtSlot
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPalette
+from PyQt5.QtWebKitWidgets import QWebView, QWebPage
+from PyQt5.QtDBus import QDBusConnection, QDBusInterface, QDBusMessage
 
-# @TODO: change appPath method
-appPath = os.path.dirname(os.path.realpath(__file__))
+
+APP_PATH = os.path.dirname(os.path.realpath(__file__))
 # @TODO: parse it from config
 DEFAULT_FM = 'dolphin'
 DEFAULT_BROWSER = "chromium-browser"
-TIMER_INTERVAL = 1000
+LEFT, TOP, WIDTH, HEIGHT = (600, 30, 680, 600)
 # @TODO: improve error reporting, then config parse will be done
 DEFAULT_MSG = "<html><body>Error in template</body></html>"
 
@@ -39,13 +38,40 @@ def normalize_command(cmd):
     return cmd
 
 
+def handle_command(cmd):
+    """
+    parsing commands from html
+    """
+    if cmd.startswith('cmd::'):
+        cmd = normalize_command(cmd)
+        print(cmd)
+        if cmd == 'exit':
+            sys.exit()
+        else:
+            Popen(cmd, shell=True)
+        return True
+    else:
+        return False
+
+
 def shell_cmd(cmd):
     try:
-        p = Popen(cmd, shell=True, stdout=PIPE)
-        output = p.stdout.read()
+        output = Popen(cmd, shell=True, stdout=PIPE).stdout.read()
     except Exception:
         output = ''
     return output
+
+
+class DBusMsgHandler(QObject):
+    callback = None
+
+    def __init__(self, callback):
+        self.callback = callback
+        super(DBusMsgHandler, self).__init__()
+
+    @pyqtSlot(QDBusMessage)
+    def handle(self, msg):
+        self.callback(msg)
 
 
 class Application:
@@ -60,44 +86,13 @@ class Application:
     html_template = None
     geometry = None
 
-    # ---------------------------------------------------------------- #
-    # events - BEGIN
-    # ---------------------------------------------------------------- #
-
     # event -  onLinkCliked
     def on_navigation(self, url):
         url = str(url.toString())
-        if self.on_command(url):
+        if handle_command(url):
             return True
         else:
-            #self.web_view.load(QUrl(url))
-            return False
-
-    # event -  onCompletePageLoading
-    def on_page_loaded(self, ok):
-        """
-        will be fixed
-        """
-        self.window.setGeometry(self.geometry[0], self.geometry[1],
-                                self.geometry[2], self.geometry[3])
-        self.window.show()
-    # ---------------------------------------------------------------- #
-    # events - END
-    # ---------------------------------------------------------------- #
-
-    def on_command(self, cmd):
-        """
-        parsing commands from html
-        """
-        if cmd.startswith('cmd::'):
-            cmd = normalize_command(cmd)
-            print(cmd)
-            if cmd == 'exit':
-                sys.exit()
-            else:
-                Popen(cmd, shell=True)
-            return True
-        else:
+            self.web_view.load(QUrl(url))
             return False
 
     def dbus_reader(self, msg):
@@ -113,36 +108,24 @@ class Application:
         self.web_view.setHtml(output)
 
     def init_dbus(self):
+        self.session_bus_connection = QDBusConnection.sessionBus()
 
-        class DBusMsgHandler(QtCore.QObject):
-
-            this = self
-            instance = None
-
-            def __init__(self, instance=0):
-                self.instance = instance
-                super(DBusMsgHandler, self).__init__()
-
-            @QtCore.pyqtSlot(QtDBus.QDBusMessage)
-            def handle(self, msg):
-                self.this.dbus_reader(msg)
-
-        self.session_bus_connection = QtDBus.QDBusConnection.sessionBus()
         service_name = 'org.mpris.MediaPlayer2.clementine'
         service_path = '/Player'
         interface_name = 'org.freedesktop.MediaPlayer'
         signal_name = 'TrackChange'
-        self.player = QtDBus.QDBusInterface(
+
+        self.player = QDBusInterface(
             service_name, service_path, interface_name,
             self.session_bus_connection)
-        self.dbus_message_handler = DBusMsgHandler()
+        self.dbus_reader(self.player.call('GetMetadata'))
+
+        self.dbus_message_handler = DBusMsgHandler(self.dbus_reader)
         self.session_bus_connection.connect(
             None, None, interface_name, signal_name,
             self.dbus_message_handler.handle)
 
-        self.dbus_reader(self.player.call('GetMetadata'))
-
-    def read_cmd_line(self):
+    def read_config(self):
         try:
             self.html_template = open('index.html', 'r').read()
         except Exception:
@@ -151,15 +134,15 @@ class Application:
 
         # @TODO: here it will be config parsing, now it's a placeholder
         try:
-            l, t, w, h = (600, 30, 680, 600)
-            self.geometry = (l, t, w, h)
-            self.html_template = self.html_template.replace('%WIDTH%', str(w))
+            self.geometry = (LEFT, TOP, WIDTH, HEIGHT)
+            self.html_template = self.html_template.replace(
+                '%WIDTH%', str(WIDTH))
         except Exception:
             print('Error in config')
-        self.html_template = self.html_template.replace('%PATH%', appPath)
+        self.html_template = self.html_template.replace('%PATH%', APP_PATH)
 
     def __init__(self):
-        self.read_cmd_line()
+        self.read_config()
 
         self.app = QApplication(sys.argv)
         self.window = QMainWindow()
@@ -181,11 +164,13 @@ class Application:
         self.web_view.page().setPalette(palette)
 
         self.web_view.page().setLinkDelegationPolicy(QWebPage.DelegateAllLinks)
-        self.web_view.loadFinished.connect(self.on_page_loaded)
         self.web_view.linkClicked.connect(self.on_navigation)
         self.web_view.setHtml(self.html_template)
 
+        self.window.setGeometry(self.geometry[0], self.geometry[1],
+                                self.geometry[2], self.geometry[3])
         self.window.setCentralWidget(self.web_view)
+        self.window.show()
 
         self.init_dbus()
 
